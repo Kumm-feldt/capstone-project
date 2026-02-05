b92c8dcd83d1f2d30221c8d5a14245db188693d8
riley-hupp <rhupp@harding.edu>
2026-02-05 13:16:05 -0600

    game logic demo. Needs time amp layers and gui aspects

--- new-game-project/game.gd
+++ new-game-project/game.gd
@@ -1,5 +1,165 @@
 extends Node2D
 
+
+signal pin_selected(pin_pos: Vector2i, player: int)
+signal pin_deselected()
+signal move_requested(from_pos: Vector2i, to_pos: Vector2i)
+signal move_validated(from_pos: Vector2i, to_pos: Vector2i, move_type: int)
+signal move_executed(from_pos: Vector2i, to_pos: Vector2i, move_type: int)
+
+# Disk signals
+signal disk_placed(octagon_pos: Vector2i, player: int)
+signal disk_flipped(octagon_pos: Vector2i, old_player: int, new_player: int)
+
+# Pin capture signals
+signal pin_captured(pin_pos: Vector2i, player: int)
+signal pin_removed(pin_pos: Vector2i, player: int)
+
+# Turn management signals
+signal turn_started(player: int)
+signal turn_ended(player: int)
+signal player_switched(old_player: int, new_player: int)
+
+# Win condition signals
+signal path_checking(start: Vector2i, end: Vector2i, player: int)
+signal path_found(start: Vector2i, end: Vector2i, player: int, path: Array)
+signal victory(player: int)
+signal game_over(winner: int)
+
+# UI update signals
+signal board_updated()
+signal valid_moves_calculated(moves: Array)
+signal display_refresh_requested()
+
+
+enum Player { NONE = 0, BLACK = 1, WHITE = 2 }
+enum MoveType { INVALID = 0, SIMPLE = 1, CAPTURE = 2, DIAGONAL = 3 }
+
+const BOARD_LAYER := 0
+const DISK_LAYER := 1
+const PIN_LAYER := 2
+const HIGHLIGHT_LAYER := 3
+
+const BLACK_DISK := 0
+const WHITE_DISK := 1
+const BLACK_PIN := 2
+const WHITE_PIN := 3
+
+@onready var tilemap: TileMap = $TileMap
+
+
+var current_player: int = Player.BLACK
+var selected_pin: Vector2i = Vector2i(-1, -1)
+
+func _ready():
+	emit_signal("turn_started", current_player)
+	emit_signal("display_refresh_requested")
+
+func _unhandled_input(event):
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		var cell := tilemap.local_to_map(tilemap.to_local(get_global_mouse_position()))
+		_handle_click(cell)
+
+
+func _handle_click(cell: Vector2i):
+	if selected_pin == Vector2i(-1, -1):
+		_try_select_pin(cell)
+	else:
+		emit_signal("move_requested", selected_pin, cell)
+		_try_move(selected_pin, cell)
+
+func _try_select_pin(cell: Vector2i):
+	var atlas := tilemap.get_cell_atlas_coords(PIN_LAYER, cell)
+	var expected := BLACK_PIN if current_player == Player.BLACK else WHITE_PIN
+	
+	if atlas.x == expected:
+		selected_pin = cell
+		emit_signal("pin_selected", cell, current_player)
+	else:
+		emit_signal("pin_deselected")
+
+func _try_move(from: Vector2i, to: Vector2i):
+	var move_type := _validate_move(from, to)
+	
+	if move_type == MoveType.INVALID:
+		emit_signal("pin_deselected")
+		selected_pin = Vector2i(-1, -1)
+		return
+	
+	emit_signal("move_validated", from, to, move_type)
+	_execute_move(from, to, move_type)
+
+func _validate_move(from: Vector2i, to: Vector2i) -> int:
+	if tilemap.get_cell_source_id(PIN_LAYER, to) != -1:
+		return MoveType.INVALID
+	
+	var delta := to - from
+	var abs := Vector2i(abs(delta.x), abs(delta.y))
+	
+	if abs == Vector2i(1, 0) or abs == Vector2i(0, 1):
+		return MoveType.SIMPLE
+	
+	if abs == Vector2i(2, 0) or abs == Vector2i(0, 2):
+		return MoveType.CAPTURE
+	
+	if abs == Vector2i(2, 2):
+		return MoveType.DIAGONAL
+	
+	return MoveType.INVALID
+
+
+func _execute_move(from: Vector2i, to: Vector2i, move_type: int):
+	tilemap.erase_cell(PIN_LAYER, from)
+	
+	var pin_tile := BLACK_PIN if current_player == Player.BLACK else WHITE_PIN
+	tilemap.set_cell(PIN_LAYER, to, 0, Vector2i(pin_tile, 0))
+	
+	if move_type == MoveType.CAPTURE:
+		var mid := (from + to) / 2
+		_handle_capture(mid)
+	
+	if move_type == MoveType.DIAGONAL:
+		_handle_disk((from + to) / 2)
+	
+	emit_signal("move_executed", from, to, move_type)
+	emit_signal("board_updated")
+	_end_turn()
+
+
+func _handle_capture(pos: Vector2i):
+	var atlas := tilemap.get_cell_atlas_coords(PIN_LAYER, pos)
+	if atlas != Vector2i(-1, -1):
+		var owner := Player.WHITE if current_player == Player.BLACK else Player.BLACK
+		tilemap.erase_cell(PIN_LAYER, pos)
+		emit_signal("pin_captured", pos, owner)
+		emit_signal("pin_removed", pos, owner)
+
+func _handle_disk(pos: Vector2i):
+	var atlas := tilemap.get_cell_atlas_coords(DISK_LAYER, pos)
+	
+	if atlas == Vector2i(-1, -1):
+		var tile := BLACK_DISK if current_player == Player.BLACK else WHITE_DISK
+		tilemap.set_cell(DISK_LAYER, pos, 0, Vector2i(tile, 0))
+		emit_signal("disk_placed", pos, current_player)
+	else:
+		var old := Player.BLACK if atlas.x == BLACK_DISK else Player.WHITE
+		var tile := BLACK_DISK if current_player == Player.BLACK else WHITE_DISK
+		tilemap.set_cell(DISK_LAYER, pos, 0, Vector2i(tile, 0))
+		emit_signal("disk_flipped", pos, old, current_player)
+
+
+func _end_turn():
+	emit_signal("turn_ended", current_player)
+	
+	var old := current_player
+	current_player = Player.WHITE if current_player == Player.BLACK else Player.BLACK
+	
+	selected_pin = Vector2i(-1, -1)
+	
+	emit_signal("player_switched", old, current_player)
+	emit_signal("turn_started", current_player)
+	emit_signal("display_refresh_requested")
+
 var PINS = [
 	[".", "o", "o", ".", "x", "x", "."],
 	["o", ".", ".", ".", ".", ".", "x"],
@@ -103,12 +263,12 @@ func isGameOver():
 	return true	
 	
 	
-func _ready() -> void:
-	move_pin('a5b4', 'X')
-	move_pin('a3a4', 'O')
-	move_pin('a6b5', 'X')
-	move_pin('a4c4', 'O')
-	print("I did something new...")
+#func _ready() -> void:
+	#move_pin('a5b4', 'X')
+	#move_pin('a3a4', 'O')
+	#move_pin('a6b5', 'X')
+	#move_pin('a4c4', 'O')
+	#print("I did something new...")
 	
 
 	
