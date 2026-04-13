3c484c0be63b0519f942c5501d4c0e32ddabc54d
Anthony Kummerfeldt <73972025+Kumm-feldt@users.noreply.github.com>
2026-04-12 21:09:43 -0500

    settings pae

--- new-game-project/scenes/NewUser/NewUser.gd
+++ new-game-project/scenes/NewUser/NewUser.gd
@@ -44,7 +44,10 @@ func register_player(username: String):
 		"points": 0,
 		"wins": 0,
 		"losses": 0,
-		"draws": 0
+		"draws": 0,
+		"picture": "default",
+		"color": "#f79617",
+		"background":"ffffff"
 	})	
 	
 	var err = http_register.request(DBService.URL,
