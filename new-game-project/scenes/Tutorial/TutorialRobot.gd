e7ae59dfb3396f8721fb48c23bdf06111647f350
riley-hupp <rhupp@harding.edu>
2026-04-15 18:07:49 -0500

    adjustments

--- new-game-project/scenes/Tutorial/TutorialRobot.gd
+++ new-game-project/scenes/Tutorial/TutorialRobot.gd
@@ -3,18 +3,16 @@ extends AnimatedSprite2D
 @onready var speech_bubble = $SpeechBubble
 @onready var dialogue_label = $SpeechBubble/TootWords
 @onready var continue_label = $SpeechBubble/Continue
-var pixel_font = preload("res://fonts/CyberpunkCraftpixPixel.otf")
+var pixel_font = preload("res://fonts/5x5_pixel/5x5_pixel.ttf")
 
 func _ready():
 	dialogue_label.add_theme_font_override("font", pixel_font)
-	dialogue_label.add_theme_font_size_override("font_size", 8)
+	dialogue_label.add_theme_font_size_override("font_size", 9)
 	continue_label.add_theme_font_override("font", pixel_font)
-	continue_label.add_theme_font_size_override("font_size", 8)
+	continue_label.add_theme_font_size_override("font_size", 9)
 	dialogue_label.add_theme_color_override("font_color", Color.BLACK)
 	continue_label.add_theme_color_override("font_color", Color.BLACK)
 	dialogue_label.autowrap_mode = TextServer.AUTOWRAP_WORD_SMART
-	dialogue_label.texture_filter = CanvasItem.TEXTURE_FILTER_NEAREST
-	continue_label.texture_filter = CanvasItem.TEXTURE_FILTER_NEAREST
 	continue_label.text = "▼ click"
 	speech_bubble.visible = false
 func wakeUp() -> void:
