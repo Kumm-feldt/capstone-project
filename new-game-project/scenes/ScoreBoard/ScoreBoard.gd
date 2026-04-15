78b226c988686b126203b93d9e4301238ca00ad9
Anthony Kummerfeldt <73972025+Kumm-feldt@users.noreply.github.com>
2026-04-15 10:33:26 -0500

    scoreboard bug fixed

--- new-game-project/scenes/ScoreBoard/ScoreBoard.gd
+++ new-game-project/scenes/ScoreBoard/ScoreBoard.gd
@@ -2,7 +2,6 @@ extends Control
 var http_check = HTTPRequest.new()
 var score_row_scene = preload("res://scenes/ScoreBoard/ScoreRow.tscn")
 @onready var vbox = $Panel/VBoxContainer/ScrollContainer/VBoxContainer
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	add_child(http_check)
