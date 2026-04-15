88a3362a7efdf9c20169691c2768d7919a4b96f5
GarrettBot <155921496+GarrettBotProgrammer@users.noreply.github.com>
2026-04-15 10:01:38 -0500

    Conveyor Belt Features
    - Robot animation recolor for consistency
    - Added conveyor belts
    - Added smug face for TOOT
    - Added MenuPanel to GameMode.tscn

---
+++ new-game-project/scenes/board/ConveyorItem.gd
@@ -0,0 +1,11 @@
+extends PathFollow2D
+
+func setup() -> void:
+	#Set the sprite for this item as one of the random items
+	var rand = randi_range(0, 7);
+	$ConveyorItem.region_rect.position = Vector2(rand * 25, 0)
+	
+	#Set the tween and off it goes!
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "progress_ratio", 1, 7);
+	pass
