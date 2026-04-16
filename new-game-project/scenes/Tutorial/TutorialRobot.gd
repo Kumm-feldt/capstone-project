69d93160a1d6978e79b06dadadfb38e3685ab009
riley-hupp <rhupp@harding.edu>
2026-04-15 22:43:33 -0500

    fixed text spacing in tutorial

--- new-game-project/scenes/Tutorial/TutorialRobot.gd
+++ new-game-project/scenes/Tutorial/TutorialRobot.gd
@@ -7,11 +7,14 @@ var pixel_font = preload("res://fonts/5x5_pixel/5x5_pixel.ttf")
 
 func _ready():
 	dialogue_label.add_theme_font_override("font", pixel_font)
-	dialogue_label.add_theme_font_size_override("font_size", 9)
+	dialogue_label.add_theme_font_size_override("font_size", 8)
 	continue_label.add_theme_font_override("font", pixel_font)
-	continue_label.add_theme_font_size_override("font_size", 9)
+	continue_label.add_theme_font_size_override("font_size", 8)
 	dialogue_label.add_theme_color_override("font_color", Color.BLACK)
-	continue_label.add_theme_color_override("font_color", Color.BLACK)
+	continue_label.add_theme_color_override("font_color", Color.WHITE)
+	dialogue_label.add_theme_constant_override("line_separation", 10)
+	dialogue_label.add_theme_constant_override("outline_size", 0)
+
 	dialogue_label.autowrap_mode = TextServer.AUTOWRAP_WORD_SMART
 	continue_label.text = "▼ click"
 	speech_bubble.visible = false
